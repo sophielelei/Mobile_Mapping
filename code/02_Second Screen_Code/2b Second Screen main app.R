8b790e03da2c15460b8ecee410172598dccbb8fd
sophielelei <60010493+sophielelei@users.noreply.github.com>
2025-02-13 13:51:33 -0500

    successfully opened 2b

--- code/02_Second Screen_Code/2b Second Screen main app.R
+++ code/02_Second Screen_Code/2b Second Screen main app.R
@@ -25,7 +25,7 @@ library(rmdformats)
 
 rm(list=ls())
 # adjust this to the appropriate folder on your system
-Workingfile <- "/Users/hlg5155/Documents/GitHub/greatrex-lab/Project Bibliometric Mapping/Mobile_Mapping/data/MainScreeningData_Screen2.rData"
+Workingfile <- "~/Desktop/mobile_money/M_mapping/data/MainScreeningData_Screen2.rData"
 
 load(Workingfile)
 
